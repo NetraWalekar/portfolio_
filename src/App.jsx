0065728f64d8b5f4085054f9063fce625f53b6b6
NetraWalekar <netrawalekar6@gmail.com>
2025-07-24 12:07:23 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./component/Hero/Hero";
+import Skills from "./component/Skills/Skills";
 
 export default function App(){
   return(
     <div className="app_container">
         <Hero></Hero>
+        <Skills></Skills>
     </div>
   )
 }
